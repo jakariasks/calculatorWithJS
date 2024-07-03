98c0f4f7de4234e9b72564d01365cd57f6b8a4c1
Md Jakaria Hasan <141640103+jakariasks@users.noreply.github.com>
2024-07-04 00:31:09 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const display = document.querySelector("#input-box");
+const btn = document.querySelectorAll("button");
+
+let btnArray = Array.from(btn);
+// console.log(btnArray)
+let str = '';
+btnArray.forEach( btn => {
+
+    btn.addEventListener('click', (e) => {
+
+        if(e.target.innerHTML == 'DEL'){
+            str = str.substring(0, str.length-1);
+            display.value = str;
+        }
+        else if(e.target.innerHTML === 'AC'){
+            str = '';
+            display.value = str;
+        }
+        else if(e.target.innerHTML == '='){
+            str = eval(str);
+            display.value = str;
+        }
+
+        else{
+            str+=e.target.innerHTML;
+             display.value = str;
+        }
+        
+
+    })
+})
